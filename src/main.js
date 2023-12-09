660f2f6786bc3f156fde3d301245229b0f00b216
skatn <87296704+skatn@users.noreply.github.com>
2023-12-10 08:05:02 +0900

    bootsrap 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
